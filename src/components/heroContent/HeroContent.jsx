ca870ffd619b71952cd87e213732bd534cc0fb21
ibrahim <ibalamy@gmail.com>
2023-11-19 12:51:28 +0100

    merge conficts resolved

--- src/components/heroContent/HeroContent.jsx
+++ src/components/heroContent/HeroContent.jsx
@@ -1,23 +1,8 @@
 import "./HeroContent.css"
 import { motion } from "framer-motion"
-import { useState } from "react"
 import { ContentVariant } from "../../animations"
 
 export const HeroContent = () => {
-  const buttonVariant = {
-    animate: {
-      x: -20,
-      transition: {
-        repeatType: 'reverse'
-      }
-    },
-    hover: {
-      scale: 1.2,
-      transition: {
-        repeat: Infinity //(keyframs)
-      }
-    }
-  }
 
   return (
     <motion.div 
@@ -29,7 +14,7 @@ export const HeroContent = () => {
         <motion.h2 exit={{y: "-100%", opacity: 0}} className="primary">welcome to chehiwaty</motion.h2>
         <p>Delve into an Enchanting Moroccan Culinary Haven Where Unparalleled Quality Harmonizes with Exquisite Taste</p>
         <div className="buttons-container">
-            <motion.button variants={buttonVariant} animate={'animate'}   whileHover={'hover'} className="primary">discover menu</motion.button>
+            <button className="primary">discover menu</button>
             <button className="neutral">make a reservation</button>
         </div>
     </motion.div>
